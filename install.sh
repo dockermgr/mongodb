6d4f9096eb96f6381d4632aa6042a7cf2cd4f996
casjay <casjay@yahoo.com>
2020-09-09 16:04:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/mongodb/ && chmod -Rf 777 /srv/docker/mongodb
+APPNAME="mongo"
+DATADIR="/srv/docker/$APPNAME"
 
+mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull mongo:latest && docker restart $APPNAME
+else
 sudo docker run -d \
---name=mongodb \
+--name="$APPNAME" \
 -p 27017:27017 \
--v /srv/docker/mongodb:/data/db \
+-v $DATADIR:/data/db \
 -e TZ=America/New_York \
 --restart unless-stopped \
 mongo:latest
+fi
